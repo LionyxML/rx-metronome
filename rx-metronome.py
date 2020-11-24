eb148694a033cbf268a578a587520c5b439907b6
Rahul M. Juliato <rahul.juliato@gmail.com>
2020-11-24 15:22:06 -0300

    Format adjusted

--- rx-metronome.py
+++ rx-metronome.py
@@ -10,24 +10,29 @@ import time
 is_playing = False
 tick_sound = sa.WaveObject.from_wave_file("./audio/tick.wav")
 
-class PlayUntilStop: 
-    def __init__(self): 
+
+class PlayUntilStop:
+    def __init__(self):
         self._running = True
-      
-    def terminate(self): 
+
+    def terminate(self):
         self._running = False
-      
+
     def run(self):
-        time_to_sleep = 60.0/time_selector.get()
-        while self._running: 
+        time_to_sleep = 60.0 / time_selector.get()
+        while self._running:
             tick_sound.play()
             time.sleep(time_to_sleep)
+
 
 def quit():
     root.destroy()
 
+
 def sobre():
-    mb.showinfo("rx-metronome", '''
+    mb.showinfo(
+        "rx-metronome",
+        """
 
     rx-metronome
 
@@ -38,11 +43,14 @@ def sobre():
     Author : Rahul M. Juliato
              rahuljuliato.com
 
-''')
+""",
+    )
+
 
 def update_selection(value):
     selected_value.set(str("%3d" % time_selector.get()) + " bpm")
     pass
+
 
 def play():
     global is_playing
@@ -53,6 +61,7 @@ def play():
         thread_play.start()
         is_playing = True
 
+
 def stop():
     global is_playing
     if is_playing == True:
@@ -61,9 +70,9 @@ def stop():
 
 
 root = tk.Tk()
-root.wm_title('rx-metronome v0.1')
+root.wm_title("rx-metronome v0.1")
 root.wm_minsize(400, 220)
-root.grid_anchor(anchor='c')
+root.grid_anchor(anchor="c")
 
 barramenu = tk.Menu(root)
 file_menu = tk.Menu(barramenu, tearoff=800)
@@ -74,8 +83,15 @@ barramenu.add_cascade(label="File", menu=file_menu)
 
 root.config(menu=barramenu)
 
-time_selector = tk.Scale(root, orient="horizontal", from_=40, to=218,
-                    showvalue=0, command=update_selection, length=250)
+time_selector = tk.Scale(
+    root,
+    orient="horizontal",
+    from_=40,
+    to=218,
+    showvalue=0,
+    command=update_selection,
+    length=250,
+)
 time_selector.grid(row=2, column=1, columnspan=2, pady=10)
 time_selector.set(80)
 
@@ -87,12 +103,9 @@ time_label.grid(row=1, column=1, columnspan=2, pady=10)
 time_label.config(font=("", 40))
 
 play_button = tk.Button(root, text="▶", command=play)
-play_button.grid(row=3, column=2, sticky='WE', pady=10)
+play_button.grid(row=3, column=2, sticky="WE", pady=10)
 
 stop_button = tk.Button(root, text="◼", command=stop)
-stop_button.grid(row=3, column=1, sticky='WE')
+stop_button.grid(row=3, column=1, sticky="WE")
 
 root.mainloop()
-
-
-
